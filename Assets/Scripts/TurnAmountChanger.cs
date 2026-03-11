81d38eadd75393956657e74c5f1477dab019ee28
AJ-FW16-CachyOS <alexander.aj.fraser@gmail.com>
2026-03-11 10:13:52 -0400

    Added Snap Turn comfort settings
    Includes a new UI settings panel and a script to toggle snap turn between 45 and 90 degrees.

---
+++ Assets/Scripts/TurnAmountChanger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
+using TMPro;
+
+public class TurnAmountChanger : MonoBehaviour
+{
+    public SnapTurnProvider mySnapTurn;
+    public TMP_Text statusText; 
+
+    private bool is45Degrees = true;
+
+    void Awake()
+    {
+        UpdateUIText();
+    }
+
+    //should hopefully toggle between settings and update the menu's text
+    public void ToggleTurnAmount()
+    {
+        if (mySnapTurn != null)
+        {
+            is45Degrees = !is45Degrees;
+
+            if (is45Degrees)
+            {
+                mySnapTurn.turnAmount = 45f;
+            }
+            else
+            {
+                mySnapTurn.turnAmount = 90f;
+            }
+            
+            UpdateUIText();
+        }
+    }
+
+    private void UpdateUIText()
+    {
+        if (statusText != null && mySnapTurn != null)
+        {
+            statusText.text = mySnapTurn.turnAmount + "°";
+        }
+    }
+}
